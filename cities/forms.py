b01f0e5a0ce479449042f6f21bcc04e0530f4ebf
Irv1nok <madmann1986@gmail.com>
2023-03-25 22:27:39 +0300

    add generic classes

--- cities/forms.py
+++ cities/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from cities.models import City
 
 
--- cities/views.py
+++ cities/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from cities.models import City
-from django.views.generic.detail import DetailView
-from .forms import CityForm, HomeCityForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
-__all__ = ('home', 'CityDetailView', )
+from cities.models import City
+from cities.forms import CityForm
+
+__all__ = ('home', 'CityDetailView', 'CityCreateView', 'CityUpdateView', 'CityDeleteView', )
 
 
 def home(request):
@@ -22,3 +24,23 @@ def home(request):
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
